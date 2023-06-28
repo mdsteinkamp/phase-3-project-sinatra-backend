4665807552268dc0a0e90727d70cc3e17fb2dbb9
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-27 19:45:49 -0700

    app controller post to clients working

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,5 @@
+require 'pry'
+
 class ApplicationController < Sinatra::Base
   set :default_content_type, 'application/json'
   
@@ -6,15 +8,21 @@ class ApplicationController < Sinatra::Base
     clients = Client.all.order("last_name")
     clients.to_json
   end
-
-  get "/policies" do
-    policies = Policy.all.order("policy_date")
-    policies.to_json
-  end
   
   get "/clients/:id" do
     policy = Client.find(params[:id])
     policy.to_json(include: :policies)
+  end
+
+  post '/clients' do
+    client = Client.create(
+      last_name: params[:last_name],
+      first_name: params[:first_name],
+      date_of_birth: params[:date_of_birth],
+      state: params[:state],
+      spouse_name: params[:spouse_name],
+    )
+    client.to_json
   end
 
 end
