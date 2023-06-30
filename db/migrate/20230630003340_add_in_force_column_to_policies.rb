d8fa24f7b7c9f1fcfd7a8cfbb556693c8d6ead9e
Matthew Steinkamp <mdsteinkamp@gmail.com>
2023-06-29 17:34:43 -0700

    added an active column to policies table with boolean value

---
+++ db/migrate/20230630003340_add_in_force_column_to_policies.rb
@@ -0,0 +1,5 @@
+class AddInForceColumnToPolicies < ActiveRecord::Migration[6.1]
+  def change
+    add_column :policies, :active, :boolean
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2023_06_15_031522) do
+ActiveRecord::Schema.define(version: 2023_06_30_003340) do
 
   create_table "clients", force: :cascade do |t|
     t.string "last_name"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 2023_06_15_031522) do
     t.string "purpose"
     t.string "rate_class"
     t.integer "client_id"
+    t.boolean "active"
   end
 
 end
